80b757280f30837c24b0bac77e763da8f5884ca4
GuoDaFeng666 <40451641+GuoDaFeng666@users.noreply.github.com>
2023-06-09 21:55:25 +0800

    Delete jjj_food_chain_shop directory

--- jjj_food_chain_shop/src/api/data.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-let DataApi = {
-  /*用户接口*/
-  getUser(data, errorback) {
-    return request._post('/shop/data.user/lists', data, errorback);
-  },
-}
-export default DataApi;
